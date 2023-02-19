746eafa8d6597372fb5fff40be270b6e51173d44
IbrahimFaisal2028 <108027026+IbrahimFaisal2028@users.noreply.github.com>
2023-02-18 19:37:38 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+gestures = "";
+tospeak = "";
 Webcam.set({
     width: 350,
     height: 300,
@@ -18,4 +20,45 @@ function modelLoaded()
 {
     console.log("Model is loaded!")
 }
+function check()
+{
+    img = document.getElementById('capture_img');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results)
+{
+if(error)
+{
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+gestures = results[0].label;
+if(gestures == "amazing")
+{
+tospeak = "it looks amazing";
+document.getElementById("update_emoji").innerHTML = "&#128076;";
+}
+else if(gestures == "best")
+{
+    tospeak = "all the best";
+    document.getElementById("update_emoji").innerHTML = "&#128077;";
+}
+else if(gestures == "victory")
+{
+    tospeak = "that was a marvelous victory";
+    document.getElementById("update_emoji").innerHTML = "&#9996;";
+}
+speak();
+}
+}
+function speak()
+{
+    var synth = window.speakSynthesis;
+    speak_data = tospeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
 
